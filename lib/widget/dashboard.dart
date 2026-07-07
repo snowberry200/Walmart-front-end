652a2b635f73cc9f46181c6372aae4a82c93f7ec
Collins Attafuah <89222300+snowberry200@users.noreply.github.com>
2026-07-07 23:39:29 +0200

    fresh update

--- lib/widget/dashboard.dart
+++ lib/widget/dashboard.dart
@@ -1,6 +1,5 @@
 // lib/screens/dashboard.dart
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:walmart/layout/layout.dart';
 
 class DashboardScreen extends StatefulWidget {
